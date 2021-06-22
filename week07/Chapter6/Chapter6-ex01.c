47ce02a85b47cd431f27c39a9a068dab889dbb90
Maudan46 <maudan2002@gmail.com>
2021-06-22 09:52:57 -0400

    Adding Chapter6-ex01

---
+++ week07/Chapter6/Chapter6-ex01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  float x1,y1,x2,y2,distance;
+  printf("Inserta las coordenada X del primer punto\n");
+  scanf("%f",&x1);
+  printf("Ingresa la coordenada y del primer punto\n");
+  scanf("%f",&y1);
+  printf("Inserta las coordenada X del segundo punto\n");
+  scanf("%f",&x2);
+  printf("Ingresa la coordenada y del segundo punto\n");
+  scanf("%f",&y2);
+
+  distance= sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+  
+  printf("La distancia entre los puntos (%f , %f) y (%f , %f) es de %f.\n",x1,y1,x2,y2,distance);
+
+  return 0;
+}
