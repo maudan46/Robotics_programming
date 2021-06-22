7c6202f5e6b825825d38733c2c5cde8ea23392b8
Maudan46 <maudan2002@gmail.com>
2021-06-22 12:48:14 -0400

    Adding Chapter7-ex03.c

---
+++ week07/Chapter7/Chapter7-ex03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//ask for the size of the file in MB
+//transform the size from MB to bytes
+//multiplicate the size in bytes for the speed of the transmission (96 characters per second)
+//Print the time
+
+int main(void) {
+  float size, time, bytes;
+  printf("Write the size of the file in MB\n");
+  scanf("%f",&size );
+  bytes= size*1048576;
+  time= bytes/960;
+
+  printf("The time required to send the file is %f seconds\n",time);
+  return 0;
+}
