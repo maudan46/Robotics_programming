f1ac641e21c7cdab091fa7698c8d026c7ceb6773
Maudan46 <maudan2002@gmail.com>
2021-06-22 11:09:09 -0400

    Adding chapter6-ex04.c

---
+++ week07/Chapter6/Chapter6-ex04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  float money;
+  int value, qua, dim, nic, pen;
+  printf("Write the amount of money in dollars (less than $1.00)\n");
+  scanf("%f",&money);
+
+  value= money*100;
+
+  qua= (value/25);
+  dim= (value%25)/10;
+  nic= ((value%25)%10)/5;
+  pen=(((value%25)%10)%5)/1;
+
+
+  printf("To get %f dollars you need %d quarters, %d dimes, %d nickles and %d pennies\n", money, qua, dim, nic, pen);
+  return 0;
+}
