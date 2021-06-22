de6477d022357b34a405891c9e96e9beaafc9d37
Maudan46 <maudan2002@gmail.com>
2021-06-22 10:46:07 -0400

    Adding Chapter6-ex03.c

---
+++ week07/Chapter6/Chapter6-ex03.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+  int number, grade;
+  int digit;
+  char sign [2];
+  
+  printf("Write the number of points.\n");
+  scanf("%d", &number);
+
+  digit= number%10;
+
+  if (digit> 0 && digit<4) {
+   sign[1]= '-';
+  }
+    if (digit> 3 && digit<8) {
+   sign[1]= ' ';
+  }
+    if (digit> 7 && digit<10) {
+   sign[1]= '+';
+  }
+  if (digit == 0) {
+   sign[1]= '+';
+
+  }
+
+  if (number<61) {
+    printf("The grade is F %c\n",sign[1]);
+  }
+   if (number<71 && number>60) {
+    printf("The grade is D %c\n",sign[1]);
+  }
+    if (number<81 && number>70) {
+    printf("The grade is C %c\n",sign[1]);
+  }
+    if (number<91 && number>80) {
+    printf("The grade is B %c\n",sign[1]);
+  }
+    if (number<101 && number>90) {
+    printf("The grade is A %c\n",sign[1]);
+  }
+  return 0;
+}
