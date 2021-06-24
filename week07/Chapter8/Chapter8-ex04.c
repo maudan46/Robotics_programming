6ef7e86f08e46d1c0a9d8fac490425e326dc7a9a
maudan46 <56905874+maudan46@users.noreply.github.com>
2021-06-24 04:57:16 -0500

    fgets y sscanf Chapter8-ex04.c

--- week07/Chapter8/Chapter8-ex04.c
+++ week07/Chapter8/Chapter8-ex04.c
@@ -2,10 +2,13 @@
 
 int main(void) {
   int n, m=0, res, lim;
+  char line[100];
   printf("Of what number you want to get the multiplication table?\n");
-  scanf("%d", &n);
+  fgets(line, sizeof(line), stdin);
+	sscanf(line, "%d", &n);
   printf("Until what number you want to multiplicate the previous number?\n");
-  scanf("%d",&lim);
+  fgets(line, sizeof(line), stdin);
+	sscanf(line, "%d", &lim);
 
   while (m<lim+1) {
     res=n*m;
