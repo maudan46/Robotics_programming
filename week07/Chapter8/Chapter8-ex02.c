2a3f743994f3b3e439159ba3e45479a609ff7143
maudan46 <56905874+maudan46@users.noreply.github.com>
2021-06-24 04:52:24 -0500

    fgets y sscanf Chapter8-ex08.c

--- week07/Chapter8/Chapter8-ex02.c
+++ week07/Chapter8/Chapter8-ex02.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
 int main(void) {
-
-  int n,count=0,num=0,div=0;
-  float resistance,temp=0,res[num];
-
-
-  printf("Of how many parallel resistors you want to calculate the total resistance?\n");
-  scanf("%d", &n);
-  while (count< n) {
-
-    count++;
-    printf("The %d resistance: \n",count);
-    scanf("%f",&res[num]);
-    num++;
-  }
-
-  temp= temp + (1/res[div]);
-  div++;
-
-  while (div<n) {
-
-    temp= temp + (1/res[div]);
-    div++;
-
-  }
-
-  resistance= 1/temp;
-
-  printf("%f\n", resistance);
-
-
-  return 0;
+	
+	int n,count=0,num=0,div=0;
+	float resistance,temp=0,res[num];
+	char line[100];
+	
+	
+	printf("Of how many parallel resistors you want to calculate the total resistance?\n");
+	fgets(line, sizeof(line), stdin);
+	sscanf(line, "%d", &n);
+	while (count< n) {
+		
+		count++;
+		printf("The %d resistance: \n",count);
+		scanf("%f",&res[num]);
+		num++;
+	}
+	
+	temp= temp + (1/res[div]);
+	div++;
+	
+	while (div<n) {
+		
+		temp= temp + (1/res[div]);
+		div++;
+		
+	}
+	
+	resistance= 1/temp;
+	
+	printf("The Rt is = %f\n", resistance);
+	
+	
+	return 0;
 }
