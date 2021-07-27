a459d84bcd3d5fa53f00f66ae81973ff96636c3a
Maudan46 <maudan2002@gmail.com>
2021-07-27 02:40:24 -0400

    Hackerrank

---
+++ week10-12/Hackerrank/Pointers in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    *a=*a+*b;
+    *b= abs(*a-((*b)*2));
+}
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d\n", a, b);
+
+    return 0;
+}
+
